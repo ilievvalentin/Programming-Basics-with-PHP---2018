10caf3fc7e36cdff2104d6b3c3a9a8fe85c60fd4
Valentin Iliev <viliev@vmware.com>
2022-05-31 05:32:41 +0300

    Commit.

--- 11. While-цикъл/08. Graduation.php
+++ 11. While-цикъл/08. Graduation.php
@@ -1,1 +1,17 @@
 <?php
+
+$name = readline();
+$grade = readline();
+$gradeNumber = 1;
+$gradeSum  = 0;
+
+while ($gradeNumber <= 12) {
+    if ($grade >= 4) {
+        $gradeSum = $gradeSum + $grade;
+        $gradeNumber++;
+    }
+    $grade = readline();
+}
+
+echo "$name graduated. Average grade: ",
+number_format(round($gradeSum / 12, 2), 2, '.', '');
